a47ce9f7d7c5769d066bf27b1f3c2373177fbea2
山地奥斯卡537 <131773717+Sean537@users.noreply.github.com>
2023-10-15 19:09:16 +0800

    Create style.js

---
+++ js/style.js
@@ -0,0 +1,13 @@
+const dock = document.querySelector('.dock');
+const tabs = dock.querySelectorAll('li');
+
+tabs.forEach(tab => {
+  tab.addEventListener('click', () => {
+    tabs.forEach(tab => {
+      tab.classList.remove('active');
+      tab.style.animation = '';
+    });
+    tab.classList.add('active');
+    tab.style.animation = 'bounce 1s infinite';
+  });
+});
