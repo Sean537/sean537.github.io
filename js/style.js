00c98819d257901e8207ef5e6b1b8ab18841dff5
山地奥斯卡537 <131773717+Sean537@users.noreply.github.com>
2023-10-19 22:35:28 +0800

    Add files via upload

--- js/style.js
+++ js/style.js
@@ -1,13 +1,13 @@
-const dock = document.querySelector('.dock');
-const tabs = dock.querySelectorAll('li');
+const dock = document.querySelector(".dock");
+const tabs = dock.querySelectorAll("li");
 
-tabs.forEach(tab => {
-  tab.addEventListener('click', () => {
-    tabs.forEach(tab => {
-      tab.classList.remove('active');
-      tab.style.animation = '';
+tabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    tabs.forEach((tab) => {
+      tab.classList.remove("active");
+      tab.style.animation = "";
     });
-    tab.classList.add('active');
-    tab.style.animation = 'bounce 1s infinite';
+    tab.classList.add("active");
+    tab.style.animation = "bounce 1s infinite";
   });
 });
